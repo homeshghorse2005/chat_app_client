b0431834d1e6dec75f4d5ce53e08e9d4c363e643
homeshghorse2005 <homeshghorse@gmail.com>
2025-12-07 20:41:11 +0530

    fix axios server

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,18 +1,18 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "production" ? "https://chat-app-server-eosin-tau.vercel.app/api" : "/api",
-  withCredentials: true,
-});
-
-
-
-
 // import axios from "axios";
 
-// const API_URL = import.meta.env.VITE_API_URL;
-
 // export const axiosInstance = axios.create({
-//   baseURL: API_URL,
+//   baseURL: import.meta.env.MODE === "production" ? "https://chat-app-server-eosin-tau.vercel.app/api" : "/api",
 //   withCredentials: true,
 // });
+
+
+
+
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});

0a7cfef3349bc46f51e353e50a8c484d9ae311ef
homeshghorse2005 <homeshghorse@gmail.com>
2025-12-10 18:09:24 +0530

    Fix issue

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -22,7 +22,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "production"
+    import.meta.env.NODE === "production"
       ? "https://chat-app-server-eosin-tau.vercel.app/api"
       : "http://localhost:5001/api",
   withCredentials: true,
